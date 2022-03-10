a32ecfc3c9a938c61bd7339a50f8a611e0deb10d
richet <richardchetwynd@gmail.com>
2022-03-11 08:58:44 +1300

    test config params

--- spec/lib/recur_crm_spec.rb
+++ spec/lib/recur_crm_spec.rb
@@ -4,4 +4,17 @@ describe RecurCrm do
       expect(c).to eq(described_class)
     end
   end
+
+  describe 'parameters' do
+    let(:fake_class) { class_double('RecurCrm') }
+    it 'is possible to set api_key' do
+      expect(fake_class).to receive(:api_key=).with('xxx')
+      fake_class.api_key = 'xxx'
+    end
+
+    it 'is possible to set base_uri' do
+      expect(fake_class).to receive(:base_uri=).with('http://example.com')
+      fake_class.base_uri = 'http://example.com'
+    end
+  end
 end
