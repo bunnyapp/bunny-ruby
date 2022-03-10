7ad8c325bc67391fa14c9e1c9a97ffd2874e7450
richet <richardchetwynd@gmail.com>
2022-03-11 10:49:20 +1300

    fxi rails generator

--- lib/generators/recur_crm/install_generator.rb
+++ lib/generators/recur_crm/install_generator.rb
@@ -1,7 +1,8 @@
+require 'rails/generators'
 require 'recur_crm'
+
 module RecurCrm
   class InstallGenerator < Rails::Generators::Base
-
     desc 'This generator creates a configuration file for the Recur CRM ruby client inside config/initializers'
     def create_configuration_file
       initializer 'recur_crm.rb' do
@@ -15,7 +16,7 @@ module RecurCrm
             c.api_key = ENV['RECURCRM_API_TOKEN']
 
             # Optional. Base URI for the Recur API
-            # c.base_uri = "https://api.recurcrm.com"
+            # c.base_uri = 'https://api.recurcrm.com'
 
           end
         CONFIG
