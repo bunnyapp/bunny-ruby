40010715d3472f4e3c3cdb7791b6120c840bcecd
richet <richardchetwynd@gmail.com>
2022-03-11 08:58:19 +1300

    encourage putting api_key in env var

--- lib/generators/recur_crm/install_generator.rb
+++ lib/generators/recur_crm/install_generator.rb
@@ -1,7 +1,6 @@
 require 'recur_crm'
 module RecurCrm
   class InstallGenerator < Rails::Generators::Base
-    argument :api_key, :base_uri
 
     desc 'This generator creates a configuration file for the Recur CRM ruby client inside config/initializers'
     def create_configuration_file
@@ -13,10 +12,10 @@ module RecurCrm
             # This is where your API key goes. You should almost certainly not have it
             # committed to source control, but instead load it from a secret store.
             # Default: nil
-            c.api_key = "#{api_key}"
+            c.api_key = ENV['RECURCRM_API_TOKEN']
 
             # Optional. Base URI for the Recur API
-            # c.base_uri = "#{base_uri}"
+            # c.base_uri = "https://api.recurcrm.com"
 
           end
         CONFIG
